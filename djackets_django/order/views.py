c2e88b6e5087c786d2f278772d54fc0e7fac1950
John <john.chang@nexus-techltd.com>
2021-06-15 20:24:51 +0800

    Add checkout function

--- djackets_django/order/views.py
+++ djackets_django/order/views.py
@@ -12,3 +12,18 @@ from rest_framework.response import Response
 
 from .models import Order, OrderItem
 from .serializers import OrderSerializer, MyOrderSerializer
+
+@api_view(['POST'])
+@authentication_classes([authentication.TokenAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def checkout(request):
+  serializer = OrderSerializer(data=request.data)
+  
+  if serializer.is_valid():
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    paid_amount = sum(item.get('quantity') * item.get('product').price for item in serializer.validated_data['items'])
+
+    try:
+      charge = stripe.Charge.create(amount=int(paid_amount * 100),currency='USD',description='Charge from Djackets',source=serializer.validated_data['stripe_token'])
+
+      serializer.save(user=request.user, paid_amount=paid_amount)
